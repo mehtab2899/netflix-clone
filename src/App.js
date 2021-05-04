714c553fd9ac05c56a3b2b61bae35ceae91d33d7
Mehtab's PC <mehtabmultani51@gmail.com>
2021-05-04 23:03:49 +0530

    feat: initialize web app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Row from "./components/Row";
+	
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>netflix clone</h1>
+			<Row title="netflix originals" />
+			<Row title="trending now" />
+		</div>
+	);
 }
 
 export default App;
